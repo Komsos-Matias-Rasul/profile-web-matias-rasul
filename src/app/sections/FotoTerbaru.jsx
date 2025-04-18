67fec46940a412f19009dbec46815a95ed69dfab
LittlebitCode <adityaahza0223@gmail.com>
2025-04-18 21:33:51 +0700

    fix: change heading

--- src/app/sections/FotoTerbaru.jsx
+++ src/app/sections/FotoTerbaru.jsx
@@ -22,7 +22,7 @@ const FotoTerbaruSection = () => {
     <div className="w-full flex justify-center ">
       <div className="w-[95%] max-w-[1300px]">
         <div className="flex flex-col items-center mb-16">
-          <h1 className="font-heading text-[2rem] text-center min-w-max text-blue-primary"><b>Kegiatan</b> Paroki</h1>
+          <h1 className="font-heading text-[2rem] text-center min-w-max text-blue-primary"><b>Galeri</b></h1>
           <div className="w-8 h-1.5 bg-blue-primary" />
           <div className="py-12 px-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
             {galeri.map((item) => (
