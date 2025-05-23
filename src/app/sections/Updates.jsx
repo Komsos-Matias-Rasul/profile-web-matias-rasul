28fee855a3064503baa5682fada6726be70b8ccf
PetrusAriaa <petrusariacr25@gmail.com>
2025-05-23 11:45:45 +0700

    prod: update berita paroki contents

--- src/app/sections/Updates.jsx
+++ src/app/sections/Updates.jsx
@@ -10,19 +10,14 @@ import 'swiper/css'
 
 const DUMMY_NEWS = [
   {
-    title: "Zaitun Edisi Natal 2024",
-    description: "Majalah digital Zaitun Edisi Natal 2024 telah terbit!\n Klik \"Zaitun\" untuk baca selengkapnya.",
-    img: "https://storage.googleapis.com/web-samara-storage/info-terbaru/zaitun-cover.jpg"
+    title: "Zaitun Edisi Paskah 2024",
+    description: "Ingin tahu kisah dibalik Tablo Paskah hingga ziarah di Gereja Katedral Osaka? Baca artikel-artikel menarik yang telah terbit di <a target='_blank' href='/zaitun' class='font-bold text-blue-secondary underline'>Zaitun Edisi Paskah 2025.</a>",
+    img: "https://storage.googleapis.com/web-samara-storage/info-terbaru/Zaitun%20Ver2%201920x1080_rev2.jpg"
   },
   {
-    title: "Audisi PSDC SAMARA",
-    description: "PSDC SAMARA mengadakan audisi untuk anggota baru. Terbuka untuk posisi Soprano, Alto, Tenor, dan Bass.",
-    img: "https://storage.googleapis.com/web-samara-storage/info-terbaru/image-4.png"
-  },
-  {
-    title: "Open Recruitment KOMSOS MATIAS",
-    description: "Bagi Sahabat Matias yang punya minat di bidang fotografi, videografi, editing & design, website/socmed management dan bakat lain di ranah publikasi, Komsos St. Matias Rasul adalah pilihan yang tepat untuk Sahabat Matias menyalurkan minatnya sekaligus bersinergi dalam pelayanan bidang pewartaan Gereja kita tercinta ini!",
-    img: "https://storage.googleapis.com/web-samara-storage/info-terbaru/image-5.png"
+    title: "Ucapan Paskah",
+    description: "Panitia Paskah 2025 Wilayah 14 mengucapkan selamat Paskah 2025 dan terima kasih kepada Pastor Paroki, Dewan Paroki, serta semua pihak yang terlibat.",
+    img: "https://storage.googleapis.com/web-samara-storage/info-terbaru/Ucapan%20Paskah.png"
   },
 ]
 
@@ -32,22 +27,22 @@ const UpdatesSection = () => {
   const swiperRef = useRef()
 
   const slideNext = () => {
-    swiperRef.current?.swiper.activeIndex != 2
+    swiperRef.current?.swiper.activeIndex != DUMMY_NEWS.length-1
       ? swiperRef.current?.swiper.slideNext() :
       swiperRef.current?.swiper.slideTo(0)
   }
   const slidePrev = () => {
     swiperRef.current?.swiper.activeIndex != 0
       ? swiperRef.current?.swiper.slidePrev() :
-      swiperRef.current?.swiper.slideTo(2)
+      swiperRef.current?.swiper.slideTo(DUMMY_NEWS.length-1)
   }
   const handleSlideChange = () => {
     setCurrentNews(swiperRef.current?.swiper.activeIndex)
   }
 
   return (
-    <>
-      <div className="w-full bg-dark-primary text-white flex flex-col md:flex-row-reverse shadow-lg">
+    <div>
+      <div className="w-full bg-dark-primary text-white flex flex-col min-h-screen md:min-h-min md:flex-row-reverse shadow-lg">
         <div className="w-full md:w-1/2">
           <Swiper
             autoplay={{
@@ -79,22 +74,25 @@ const UpdatesSection = () => {
             }
           </Swiper>
         </div>
-        <div className="flex justify-end w-full md:w-1/2 max-w-[650px]">
-          <div className="p-8 text-white flex flex-col justify-between">
-            <div>
-              <p className="font-heading text-blue-secondary mb-2 md:mb-4 font-bold tracking-[5px] md:tracking-[10px] text-sm md:text-base">BERITA PAROKI</p>
-              <h1 className="font-heading text-2xl md:text-5xl font-bold line-clamp-2 mb-2 md:mb-4 leading-normal">{DUMMY_NEWS[currentNews].title}</h1>
-              <p className="line-clamp-3 md:line-clamp-2 text-sm md:text-base">{DUMMY_NEWS[currentNews].description}</p>
-            </div>
-            <div className="flex gap-4 justify-center lg:justify-end pt-5">
+        <div className="flex justify-start md:justify-end w-full md:w-1/2 max-w-[650px]">
+          <div className="p-8 h-full text-white flex flex-col justify-between">
+            <div className="flex md:hidden gap-4 justify-center lg:justify-end pb-5">
               <Button onPress={slidePrev} disableRipple isIconOnly variant="ghost" radius="full" color="primary" size="sm">{"<"}</Button>
               <Button onPress={slideNext} disableRipple isIconOnly variant="ghost" radius="full" color="primary" size="sm">{">"}</Button>
             </div>
-            {/* <Button as={Link} href="/zaitun" variant="ghost" radius="sm" className="text-white mt-8 data-[hover=true]:border-blue-secondary data-[hover=true]:!bg-blue-secondary">Kunjungi</Button> */}
+            <div>
+              <p className="font-heading text-blue-secondary mb-2 md:mb-4 font-bold tracking-[5px] md:tracking-[10px] text-sm md:text-base">BERITA PAROKI</p>
+              <h1 className="font-heading text-4xl md:text-5xl font-bold line-clamp-2 mb-2 md:mb-4 leading-normal">{DUMMY_NEWS[currentNews].title}</h1>
+              <p className="line-clamp-5 md:line-clamp-5 text-lg md:text-lg" dangerouslySetInnerHTML={{__html:DUMMY_NEWS[currentNews].description}}></p>
+            </div>
+            <div className="hidden md:flex gap-4 justify-center lg:justify-end pt-5">
+              <Button onPress={slidePrev} disableRipple isIconOnly variant="ghost" radius="full" color="primary" size="sm">{"<"}</Button>
+              <Button onPress={slideNext} disableRipple isIconOnly variant="ghost" radius="full" color="primary" size="sm">{">"}</Button>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
