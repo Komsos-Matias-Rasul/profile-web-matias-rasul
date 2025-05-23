1a7cfe5afa975ff257d1316d1c729d80bf66329d
PetrusAriaa <petrusariacr25@gmail.com>
2025-05-23 11:04:27 +0700

    style: fix hanging blue overlay on mobile view

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
             fill />
         </div>
       </div>
-      <div className="bg-blue-950/50 w-full h-full max-h-[720px] absolute flex justify-center">
+      <div className="bg-blue-950/50 w-full h-screen max-h-[720px] absolute flex justify-center">
         <div className="w-[90%] max-w-[1300px] text-white h-full flex flex-col items-center justify-center  gap-4">
           <h1 className="font-heading text-4xl sm:text-5xl md:text-6xl lg:text-6xl font-black text-center">SELAMAT DATANG</h1>
           <div>
