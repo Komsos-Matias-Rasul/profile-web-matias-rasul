5ad40eae915cd533694598943082ad1526247656
kderrylo <derrylzodxyt@gmail.com>
2026-01-19 12:30:03 +0700

    fix: rollback manual wkwk

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,48 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "api.parokikosambibaru.or.id",
-        pathname: "/api/img/**",
+        protocol: 'https',
+        hostname: 'api.parokikosambibaru.or.id',
+        port: '',
+        pathname: '/api/img/**',
       },
       {
-        protocol: "https",
-        hostname: "api.parokikosambibaru.or.id",
-        pathname: "/api/ads/**",
+        protocol: 'https',
+        hostname: 'api.parokikosambibaru.or.id',
+        port: '',
+        pathname: '/api/ads/**',
       },
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8080",
-        pathname: "/**",
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8080',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "web-admin-eta-gray.vercel.app",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'web-admin-eta-gray.vercel.app',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "storage.googleapis.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
-
   async redirects() {
     return [
       {
         source: "/zaitun",
         destination: "/zaitun/2025/10",
-        permanent: false,
-      },
-    ];
-  },
+        permanent: false
+      }
+    ]
+  }
 };
 
 export default nextConfig;
